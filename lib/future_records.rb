dc9392cf2a925afe4a82cc5214e389c1c28be450
Akira Matsuda <ronnie@dio.jp>
2018-04-03 21:49:58 +0900

    FutureRecords.future that runs the given block in a new Thread

--- lib/future_records.rb
+++ lib/future_records.rb
@@ -23,6 +23,27 @@ module FutureRecords
       @records
     end
   end
+
+  class << self
+    def future(&block)
+      Result.new(&block)
+    end
+  end
+
+  class Result
+    def initialize
+      @thread = Thread.new do
+        ActiveRecord::Base.connection_pool.with_connection do
+          @records = yield
+        end
+      end
+    end
+
+    def records
+      @thread.join
+      @records
+    end
+  end
 end
 
 ActiveSupport.on_load :active_record do
